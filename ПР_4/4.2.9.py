553feb0afdd7ebb14c2658fb42d42791e8be2fed
Olef-Rivel <90057465+Olef-Rivel@users.noreply.github.com>
2025-05-22 00:52:56 +0300

    Update 4.2.9.py

--- ПР_4/4.2.9.py
+++ ПР_4/4.2.9.py
@@ -1,27 +1,62 @@
-# Из десятичной
-def to_base_n(number, base):
-    digits = "0123456789ABCDEF"  # Поддержка систем до 16-ричной
-    result = ""
-    
+# Выполнил: Машук Ю.Н.
+# Группа: МС-32
+
+
+
+LETTERS_EX = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
+DIGITS_EX = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
+
+
+def to_base(number, base):
+    """Преобразовать десятичное число 'number' в с.с. 'base'.
+
+    Параметры:
+        number (int): десятичное число;
+        base (int): система счисления.
+
+    Результат:
+        str: число в с.с. 'base'."""
+    if number == 0:
+        return "0"
+
+    digits = []
     while number > 0:
-        result = digits[number % base] + result
+        remainder = number % base
+        if remainder < 10:
+            digits.append(str(remainder))  # от 0 до 9
+        else:
+            digits.append(LETTERS_EX[remainder])  # от A до F
         number //= base
-    
-    return result if result else "0"  # Если число 0, возвращаем "0"
 
-# Пример использования:
-num = int(input("Введите число в 10-й системе: "))
-base = int(input("Введите систему счисления (2-16): "))
+    return ''.join(digits[::-1])
 
-print(f"{num} в системе {base}: {to_base_n(num, base)}")
 
-# В десятиричну
+def from_base(number, base):
+    """Преобразовать число в с.с. 'base' в десятичное число.
+    Результат:
+        int: число в 10-й с.с."""
+    number = number.upper()
+    result = 0
+    for i, digit in enumerate(number[::-1]):
+        if digit.isdigit():
+            value = int(digit)
+        else:
+            value = DIGITS_EX[digit]
+        result += value * (base ** i)
 
-def from_base_n(number_str, base):
-    return int(number_str, base)
+    return result
 
-# Пример использования:
-num_str = input("Введите число в N-й системе: ")
-base = int(input("Введите систему счисления (2-16): "))
+decimal_number = 34
+base = 16
 
-print(f"{num_str} из системы {base} в 10-й: {from_base_n(num_str, base)}")
+#из 10-й системы в 16-ю
+converted = to_base(decimal_number, base)
+print(f"{decimal_number} в 16-й системе: {converted}")
+
+# из 16-й системы в 10-ю
+converted_back = from_base(converted, base)
+print(f"{converted} в 10-й системе: {converted_back}")
+# --------------
+# Пример вывода:
+#
+# Нет
