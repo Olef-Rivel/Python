c086da4cb9d07e1c6dcbb20d245393710c1a4667
Olef-Rivel <90057465+Olef-Rivel@users.noreply.github.com>
2025-05-22 00:47:52 +0300

    Update and rename 4.2.7.py to РПОС_ПР№4.2.7.py

--- ПР_4/4.2.7.py
+++
@@ -1,8 +0,0 @@
-# Ввод предложений
-n = int(input("Введите количество предложений: "))
-sentences = [input(f"Введите предложение {i+1}: ") for i in range(n)]
-
-# Подсчет предложений с цифрами
-count = sum(1 for sentence in sentences if any(char.isdigit() for char in sentence))
-
-print(f"Количество предложений с цифрами: {count}")
