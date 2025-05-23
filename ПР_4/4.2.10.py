5a505fc1e9079be8cd633b79559908050eafbaa8
Olef-Rivel <90057465+Olef-Rivel@users.noreply.github.com>
2025-05-23 21:30:19 +0300

    Update 4.2.10.py

--- ПР_4/4.2.10.py
+++ ПР_4/4.2.10.py
@@ -1,16 +1,31 @@
-def count_characters(text):
-    text = text.lower()  # Игнорируем регистр
-    char_count = {}
 
-    for char in text:
-        if char.isalpha():  # Учитываем только буквы
-            if char in char_count:
-                char_count[char] += 1
-            else:
-                char_count[char] = 1
+# Выполнил: Машук Ю.Н.
+# Группа: МС-32
 
-    for char, count in char_count.items():
-        print(f"{char} = {count}")
 
-sentence = input("Введите предложение: ")
-count_characters(sentence)
+
+def sentence_stats(sentence):
+    """Вернуть символьную статистику 'sentence'. Регистр не учитывается."""
+    
+    sentence_stats_dict = {}  # Переименуем, чтобы избежать путаницы
+
+    sentence = sentence.lower()  # Приводим текст к нижнему регистру
+    
+    for char in sentence:
+        if char in sentence_stats_dict:
+            sentence_stats_dict[char] += 1
+        else:
+            sentence_stats_dict[char] = 1
+
+    return sentence_stats_dict  # Возвращаем результат вместо print
+
+
+s = input("Введите предложение: ")
+print(sentence_stats(s))  # Передаём введённое пользователем предложение
+
+
+# --------------
+# Пример вывода:
+#
+# Введите предложение: мама МЫла РамУ
+# {'л': 1, 'р': 1, 'у': 1, 'м': 4, 'а': 4, 'ы': 1, ' ': 2}
